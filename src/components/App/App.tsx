a8091b5195095b2568143d7e98598e07ad4ca8c0
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-01-13 14:36:46 +0300

    add/footer

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,5 @@
 import React, {FC} from 'react';
+import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import styles from './App.module.css';
 
@@ -8,6 +9,7 @@ const App:FC = () => {
   return (
     <div className={styles.page}>
       <Header/>
+      <Footer/>
     </div>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import { FC } from "react";
 import styles from "./Header.module.css";
 
 const Header: FC = () => {
