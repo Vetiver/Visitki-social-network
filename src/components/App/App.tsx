3a577ce2066afa335716a345606ef0f56929c971
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-01-14 23:40:25 +0300

    небольшие фиксы по стилям и чистка лишнего

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,19 +1,21 @@
-import { FC } from "react";
-import Login from "../../pages/Login/Login";
-import Footer from "../Footer/Footer";
-import Header from "../Header/Header";
-import styles from "./App.module.css";
+import {FC} from 'react';
+import Login from '../../pages/Login/Login';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import styles from './App.module.css';
 
-const App: FC = () => {
+
+const App:FC = () => {
+
   return (
     <div className={styles.page}>
-      <Header />
+      <Header/>
       <div className={styles.main}>
-        <Login />
+      <Login/>
       </div>
-      <Footer />
+    <Footer/>
     </div>
   );
-};
+}
 
 export default App;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect } from "react";
+import { FC } from "react";
 import { TButton } from "../../utils/types";
 import styles from "./Button.module.css";
 
