50c001e1f81ffeda46ba4a0a257ce9ded2e94b91
Vetiver <dmitriy_serga@mail.ru>
2023-01-15 15:35:31 +0300

    фикс конфликта

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-
 import "./App.css";
-import { SearchPage } from "./SearchPage/SearchPage";
+
 
 function App() {
   return (
     <div className="App">
-      <SearchPage />
+      
     </div>
   );
 }
