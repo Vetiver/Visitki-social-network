362dbede548db86b79ef3a14508d27508955b5ea
theverybusybee <v_antonchik@mail.ru>
2023-01-16 00:35:45 +0300

    styles for inputs added

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,44 +1,165 @@
-import React from 'react';
-import styles from "./Profile.module.css"
+import React from "react";
+import styles from "./Profile.module.css";
+import { ReactComponent as ArrowDown } from "../../images/logo/arrow-down.svg";
+import { ReactComponent as ArrowUp } from "../../images/logo/arrow-up.svg";
+import { ReactComponent as Calendar } from "../../images/logo/calendar.svg";
+import { ReactComponent as Clip } from "../../images/logo/clip.svg";
+
+const dataForSelect: Array<string> = ["moscow", "minsk", "kyiv", "moscow", "minsk", "kyiv"];
+const dataForLine = ["серьезный", "несерьезный"];
+
+type TSelect = {
+  data: Array<string>;
+};
+function Select({ data }: TSelect) {
+  return (
+    <div className={styles.select}>
+      <ul className={styles.select__list}>
+        {data.map((el) => (
+          <li className={styles.select__item}>{el}</li>
+        ))}
+				 <li className={`${styles.select__item} ${styles.select__item_selected}`}>{'fdvmkl'}</li>
+				
+      </ul>
+    </div>
+  );
+}
 
 function Profile() {
   return (
-    <main  >
-			<div className={styles.photo__conteiner}>
-			<h4 className={styles.photo__load}>Загрузите фото*</h4>
-				<p className={styles.photo__size}>(размер не менее 440х440)</p>
-			</div>
-				<form className={styles.form} action="">
-				<label htmlFor="">Дата рождения *</label>
-				<input type="text" />
-				<label htmlFor="">Выберите город *</label>
-				<input type="text" />
-				<label htmlFor="">Ник в телеграм</label>
-				<input type="text" />
-				<label htmlFor="">Ник на гитхабе</label>
-				<input type="text" />
-				<label htmlFor="">Выберите шаблон</label>
-				<input type="text" />
-				<label htmlFor="">Девиз, цитата</label>
-				<input type="text" />
-				<label htmlFor="">Увлечения, досуг, интересы</label>
-				<input type="text" />
-				<p>Рекомендуемый размер фото 230х129</p>
-				<input type="text" />
-				<label htmlFor="">Семья, статус, домашние животные</label>
-				<input type="text" />
-				<p>Рекомендуемый размер фото 230х129</p>
-				<input type="text" />
-				<label htmlFor="">Из какой сферы пришёл? Кем работаешь?</label>
-				<input type="text" />
-				<label htmlFor="">Почему решил учиться на веб-разработчика?</label>
-				<input type="text" />
-				<p>Поля, отмеченные звездочкой, обязательны * для заполнения</p>
-        <button className={styles.profile__button}>
-            <span className={styles.profile__buttonText}>Сохранить</span>
-        </button>     
-				</form>
-				 
+    <main className={styles.main}>
+      <div className={styles.photo__container}>
+        <h4 className={styles.photo__load}>Загрузите фото*</h4>
+        <p className={styles.photo__size}>(размер не менее 440х440)</p>
+      </div>
+      <form className={styles.form} action="">
+        <div className={styles.input__container}>
+          <p className={styles.input__title}> Дата рождения *</p>
+          <label className={styles.input__label} htmlFor="">
+            <input className={styles.input} type="date" />
+            <Calendar className={styles.input__icon} />
+          </label>
+        </div>
+
+        <div className={styles.input__container}>
+          <p className={styles.input__title}> Выберите город *</p>
+          <div className="styles.select__container">
+            <select className={styles.select__tag} name="" id=""></select>
+            <Select data={dataForSelect} />
+            <ArrowDown className={styles.input__icon} />
+            {/* <ArrowUp className={styles.input__icon}/> */}
+          </div>
+        </div>
+
+        <div className={styles.input__container}>
+          <p className={styles.input__title}> Ник в телеграм</p>
+          <label className={styles.input__label} htmlFor="">
+            <input
+              className={styles.input}
+              placeholder="@example"
+              type="text"
+            />
+          </label>
+        </div>
+
+        <div className={styles.input__container}>
+          <p className={styles.input__title}> Ник на гитхабе</p>
+          <label className={styles.input__label} htmlFor="">
+            <input
+              className={styles.input}
+              placeholder="@example"
+              type="text"
+            />
+          </label>
+        </div>
+
+        <div className={styles.input__container}>
+          <p className={styles.input__title}> Выберите шаблон *</p>
+          <select className={styles.select__tag} name="" id="">
+            <option value="серьезный">серьезный</option>
+            <option value="несерьезный">несерьезный</option>
+          </select>
+          <ArrowDown className={styles.input__icon} />
+          {/* <ArrowUp className={styles.input__icon}/> */}
+        </div>
+
+        <div className={styles.input__container}>
+          <p className={styles.input__title}> Девиз, цитата</p>
+          <textarea
+            className={styles.textarea}
+            placeholder="Не более 100 символов"
+            maxLength={100}
+          ></textarea>
+        </div>
+
+        <div className={styles.input__container}>
+          <p className={styles.input__title}> Увлечения, досуг, интересы</p>
+          <label
+            className={`${styles.input__label} ${styles.input__label_type_file}`}
+            htmlFor=""
+          >
+            <input className={styles.input} type="file" />
+            <Clip className={styles.input__icon} />
+          </label>
+          <p className={styles.input__caption}>
+            Рекомендуемый размер фото 230х129
+          </p>
+          <textarea
+            className={styles.textarea}
+            placeholder="Не более 300 символов"
+            maxLength={100}
+          ></textarea>
+        </div>
+
+        <div className={styles.input__container}>
+          <p className={styles.input__title}>
+            Семья, статус, домашние животные
+          </p>
+          <label
+            className={`${styles.input__label} ${styles.input__label_type_file}`}
+          >
+            <input className={styles.input} type="file" />
+            <Clip className={styles.input__icon} />
+          </label>
+          <p className={styles.input__caption}>
+            Рекомендуемый размер фото 230х129
+          </p>
+          <textarea
+            className={styles.textarea}
+            placeholder="Не более 300 символов"
+            maxLength={100}
+          ></textarea>
+        </div>
+
+        <div className={styles.input__container}>
+          <p className={styles.input__title}>
+            Из какой сферы пришёл? Кем работаешь?
+          </p>
+
+          <textarea
+            className={styles.textarea}
+            placeholder="Не более 300 символов"
+            maxLength={100}
+          ></textarea>
+        </div>
+
+        <div className={styles.input__container}>
+          <p className={styles.input__title}>
+            Почему решил учиться на веб-разработчика?
+          </p>
+
+          <textarea
+            className={styles.textarea}
+            placeholder="Не более 300 символов"
+            maxLength={100}
+          ></textarea>
+        </div>
+
+        <p className={styles.warning}>
+          Поля, отмеченные звездочкой, обязательны для&nbsp;заполнения
+        </p>
+        <button className={styles.profile__button}>Сохранить</button>
+      </form>
     </main>
   );
 }
