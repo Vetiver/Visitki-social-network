2de036118dc72cf965aee3f9382edafa934e96ab
Vetiver <dmitriy_serga@mail.ru>
2023-01-16 15:11:12 +0300

    комит

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,6 +3,7 @@ import Profile from '../../pages/Profile/Profile';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import styles from './App.module.css';
+import { Switch, Route } from "react-router-dom";
 
 
 const App:FC = () => {
@@ -10,7 +11,11 @@ const App:FC = () => {
   return (
     <div className={styles.page}>
       <Header/>
-      <Profile/>
+      <Switch>
+        <Route path="/profile" exact={true}>
+          <Profile/>
+        </Route>
+      </Switch>
       <Footer/>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from "react-router-dom";
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
