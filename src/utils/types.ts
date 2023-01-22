2657df06663e1c0eab39a374c99a24b68d271f1f
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2023-01-22 21:01:02 +0300

    add types for data api

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -15,10 +15,109 @@ export type TAuth = {
   userData: any | null;
 };
 
-export type TContext = {
-  state: TAuth;
-  setState?: Dispatch<SetStateAction<TAuth>> | TFcVoid;
-} | any;
+export type TContext =
+  | {
+      state: TAuth;
+      setState?: Dispatch<SetStateAction<TAuth>> | TFcVoid;
+    }
+  | any;
 
-export type TProfileDataRequest = {
-}
+//Работа с API бекенда
+//Краткая информация о студенте
+export type TUserDataDetail = {
+  email: string;
+  cohort: string;
+  _id: string;
+  createdAt: number;
+  updatedAt: number;
+  name: string;
+};
+
+//Получение списка студентов
+export type TUsersDataDetail = {
+  total: number;
+  items: TUserDataDetail[];
+};
+
+//Информация для профиля пользователя
+export type TProfile = {
+  name: {
+    value: string;
+  };
+  photo: {
+    value: string;
+  };
+  city: {
+    value: string;
+  };
+  birthday: string;
+  quote: string;
+  telegram: string;
+  github: string;
+  template: string;
+};
+
+//Информация о личной жизни студента
+export type TProfileInfo = {
+  hobby: {
+    value: string;
+  };
+  status: {
+    value: string;
+  };
+  job: {
+    value: string;
+  };
+  edu: {
+    value: string;
+  };
+};
+
+//Полная детальная информации о студенте по ID (тип для get и patch запросов)
+export type TProfileID = {
+  email: string;
+  cohort: string;
+  _id: string;
+  createdAt: number;
+  updatedAt: number;
+  profile: TProfile;
+  info: TProfileInfo;
+  reactions: number;
+};
+
+//Реакции конкретного пользователя
+export type TProfileReactions = {
+  text: string;
+  _id: string;
+  from: {
+    value: string;
+  };
+  target: string;
+};
+
+//Получение реакций пользователей для конкретного пользователя
+export type TReactionsRequest = {
+  total: number;
+  items: TProfileReactions[];
+};
+
+//Комментарий пользователя
+export type TComment = {
+  text: string;
+  _id: string;
+  from: {
+    value: string;
+  };
+  target: string;
+  to: {
+    _id: string;
+    name: string;
+    email: string;
+  };
+};
+
+//Получение всех комментариев пользователей
+export type TCommentsRequest = {
+  total: number;
+  items: TComment[];
+};
