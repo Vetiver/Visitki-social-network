62f4295071454e7128b059166c72a924d7c976db
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2023-01-24 01:54:06 +0300

    xxxxxxxxxxxxxx

--- src/components/AccountBox/AccountBox.tsx
+++ src/components/AccountBox/AccountBox.tsx
@@ -4,18 +4,21 @@ import { AuthContext } from "../../services/AuthContext";
 import styles from "./AccountBox.module.css";
 
 const AccountBox: FC = (): JSX.Element => {
-  const { state } = useContext(AuthContext)
-  
+  const { state } = useContext(AuthContext);
   return (
     //Тут будет не профиль
-    <ProtectedLink to={"profile"} className={styles.accountBox}>
-      <img
-        className={styles.profilePhoto}
-        src={state.userData.profile.photo}
-        alt="Фото профиля"
-      />
+    <ProtectedLink to={state.isAdmin ? '/admin' : `/details/:${state.id}`} className={styles.accountBox}>
+      {!state.isAdmin ? (
+        <img
+          className={styles.profilePhoto}
+          src={state.userData.profile.photo}
+          alt="Фото профиля"
+        />
+      ) : (
+        <div className={styles.profilePhotoAdmin} />
+      )}
       <p className={styles.profileName}>
-        {state.userData.profile.name}
+        {!state.isAdmin ? state.userData.profile.name : "Панель администратора"}
       </p>
     </ProtectedLink>
   );
--- src/pages/AdminPage/AdminPage.tsx
+++ src/pages/AdminPage/AdminPage.tsx
@@ -1,8 +1,18 @@
 import styles from "./AdminPage.module.css";
 import { Search } from "../../components/Search/Search";
-import { FC } from "react";
+import { FC, useContext } from "react";
+import { AuthContext } from "../../services/AuthContext";
+import { Navigate } from "react-router";
+import { useLocation } from "react-router";
 
-export const AdminPage:FC = (): JSX.Element => {
+export const AdminPage: FC = (): JSX.Element => {
+  const location = useLocation();
+  const { state } = useContext(AuthContext);
+
+  if (!state.isAdmin) {
+    return <Navigate to={"/"} state={{ from: location }} />
+  }
+
   return (
     <section className={styles.search_page}>
       <Search />
